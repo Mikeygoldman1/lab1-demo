84bdd1be8edb242746c0c7e471627c4c6ac8e00d
Jason Mervis <jasonmervis1@gmail.com>
2021-08-20 13:24:55 +0200

    Exercise 3.1

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -6,6 +6,13 @@
 
 using namespace std; // cout, endl, complex are all part of this namespace
 
+/*
+    Exercise 3.1:
+    One would need to use operator overloading in order for the commented line of code to work.
+    This is because the operator '++' cannot be used for the complex class without
+    the use of operator overloading.
+*/
+
 int main()
 {
 	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
@@ -14,8 +21,8 @@ int main()
 	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
 
 	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
+	cout << "Or in a more familiar form: " << answer.real()
+			<< " + " << answer.imag() << "j"
 			<< endl	<< endl;
 
 	// answer++;
